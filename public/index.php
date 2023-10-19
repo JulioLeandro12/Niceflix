0701d27a824a8897e3e7dcef5b381a3b66ac2d6b
Julin <julioleandromds12@gmail.com>
2023-10-19 16:49:39 -0300

    Só mudei quase tudo apenas

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>NiceFlix</title>
 </head>
 
@@ -13,131 +14,45 @@
         <img src="../image/Niceflix logo.png" alt="niceflix.png" style="width: 115px; height: 58px; margin-left: 60px;">
         <div class="search-container">
             <input type="text" id="search" placeholder="Buscar...">
-            <button type="submit" id="search-button"><i class="fas fa-search"></i></button>
+            <button type="submit" class="search-button" hover> <img src="../image/icon_search.png" alt="icon search" style="width: 20px; height: 20px; align-items: center;"> </button>
         </div>
 
     </div>
 
-    <div class="navBarGenero">
-        <h2> <img src="../image/Genero.png" alt="Genero.png" style="width: 80px; height: 25px"> </h2>
-        <div id="generos">
-            
-        </div>
-        <br>
-    </div>
+<div class="primeira-entrada" style="background-image: url(../image/Wallpaper-confia.png); width: 100%; height: 100%;" >
     
-    <div>
-        <div class="navBarSeries">
-        <h2> <img src="../image/series.png" alt="series.pnh" style="width: 80px; height: 25px"> </h2>
-        </div>
+    <div style="background-color:black;">    
+            <!-- Grupo de filmes -->
+            <div style="background-color: black;">
+                <div class="navBarFilmes">
+                    <h2> Filmes mais procurados </h2>
+                </div>
+                <div name="filmes" style="margin-left: 20px;">
+                    <img src="https://br.web.img3.acsta.net/r_1280_720/pictures/23/09/17/17/37/4230804.jpg" alt="Som da liberdade" class="seriesHover" style="width: 140px; height: 190px; cursor: pointer;">
+                    <img src="https://media.fstatic.com/6f3tStsA0s0DQk4hu0D-aylS4Xo=/210x312/smart/filters:format(webp)/media/movies/covers/2023/06/cats_Q3T3Ucz.jpg" alt="FNAF" class="seriesHover" style=" margin-left: 10px; width: 140px; height: 190px; cursor: pointer;">
+                    <img src="https://trilhadomedo.com/wp-content/uploads/2022/04/a-freira-2-the-nun-2.jpg" alt="A freira 2" class="seriesHover" style=" margin-left: 10px; width: 140px; height: 190px; cursor: pointer;">
+                    <img src="https://www.folhadelondrina.com.br/img/Artigo-Destaque/3230000/E-o-espectador-infiltrado-ataca-novamente-Estreia-0323337500202305271241.jpg?xid=5938844" alt="A Pequena Sereia" class="seriesHover" style=" margin-left: 10px; width: 140px; height: 190px; cursor: pointer;">
+                </div>
+            </div>    
+            
+            <!-- Grupo de series -->
+            <div style="background-color: black">
+                <div class="navBarSeries">
+                    <h2> Series recomendadas </h2>
+                </div>
+                <div id="series" style="margin-left: 20px;">
+                        <img src="../image/series/black mirror.jpg" alt="serie1" class="seriesHover" style="width: 140px; height: 190px; cursor: pointer">
+                        <img src="../image/series/breaking-bad.jpg" alt="serie2" class="seriesHover" style="margin-left: 10px; width: 140px; height: 190px; cursor: pointer;">
+                        <img src="../image/series/How_I_Met_Your_Mother.jpg" alt="serie3" class="seriesHover" style="margin-left: 10px; width: 140px; height: 190px; cursor: pointer; ">
+                        <img src="../image/series/supernatural.jpg" alt="serie4" class="seriesHover" style="margin-left: 10px; width: 140px; height: 190px; cursor: pointer; "> 
+                </div>
+            </div>
     </div>
-    <div id="series" style="margin-left: 20px;">
-            <img src="../image/films/gatinho teste.jpeg" alt="filme gatinho" style="width: 100px; height: 140px; cursor: pointer">
-            <img src="../image/films/anime com cerveja.jpg" alt="filme2" style="margin-left: 10px; width: 100px; height: 140px; cursor: pointer;">
-            <img src="../image/films/filme011.jpg" alt="filme3" style="margin-left: 10px; width: 100px; height: 140px; cursor: pointer;">
-            <img src="../image/" alt="">
-    </div>
-
+</div>
+    
 </body>
 
-
 <script>
-
-
 </script>
-
-<style>
-    body {
-        margin: 0%;
-        background-color: black;
-    }
-    .navBar {
-        background-color: black;
-        width: 100%;
-        height: 90px;
-        text-align: left;
-        display: flex;
-        justify-content: left;
-        align-items: center;
-    }
-
-    i#burguer {
-        background-color: black;
-        display: block;
-        color: #333;
-        margin-left: 15px;
-        cursor: pointer;
-    }
-
-    .material-symbols-outlined {
-        font-variation-settings:
-        'FILL' 0,
-        'wght' 400,
-        'GRAD' 0,
-        'opsz' 24
-    }
-
-    .navBarGenero {
-        background-color: black;
-        width: 100%;
-        height: 20%;
-        margin-left: 20px;
-    }
-
-    .navBarSeries {
-        background-color: black;
-        width: 100%;
-        height: 20%;
-        margin-left: 20px;
-    }
-
-    .generos {
-        
-    }
-
-    .series {
-        width: 10px;
-        height: 20px;
-        margin-left: 20px;
-        position: absolute;
-    }
-
-
-    /* Estilo para a barra de pesquisa */
-    .search-container {
-        margin-left: 480px;
-        display: flex;
-        background-color: #333;
-        padding: 5px, 2px;
-        border-radius: 5px;
-    }
-
-    /* Estilo para o campo de entrada de texto */
-    #search {
-        width: 400px;
-        flex-grow: 1;
-        padding: 5px;
-        border: none;
-        background-color: transparent;
-        color: white;
-        outline: none;
-    }
-
-    /* Estilo para o botão de pesquisa */
-    #search-button {
-        background-color: #C20F08; /* Cor vermelha da Netflix */
-        border: none;
-        padding: 5px 15px;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-
-    /* Estilo para o ícone de pesquisa dentro do botão */
-    i#search-button {
-        color: white;
-    }
-
-
-</style>
 
 </html>
